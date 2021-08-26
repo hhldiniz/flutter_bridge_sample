b81fe4baa09bf8f473409c90fdc356ed68f85896
Hugo Diniz <hhldiniz@gmail.com>
2021-08-26 20:12:51 -0300

    Adding web version (workaround way)

--- lib/bloc/home_bloc.dart
+++ lib/bloc/home_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_bridge_sample/bloc/base_bloc.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class HomeBloc extends BaseBloc {
   StreamController<String> _platformStreamController =
@@ -10,9 +11,12 @@ class HomeBloc extends BaseBloc {
   Stream<String> get platformStream => _platformStreamController.stream;
 
   retrievePlatformName(MethodChannel channel) {
-    channel
-        .invokeMethod("getPlatformName")
-        .then((value) => _platformStreamController.sink.add(value));
+    if (kIsWeb) {
+      _platformStreamController.sink.add("Web");
+    } else
+      channel
+          .invokeMethod("getPlatformName")
+          .then((value) => _platformStreamController.sink.add(value));
   }
 
   @override
